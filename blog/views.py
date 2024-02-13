f88f2093c64fd922ddbbdd65cafa2449c09509e2
okiromosh <moses.okiror93@gmail.com>
2024-02-13 16:20:30 +0300

    markdown feature

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,11 @@ def index(request):
         'posts': posts,
     }
     return render(request, 'blog/index.html', context)
+
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/detail.html', context)
